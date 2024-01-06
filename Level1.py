f38c5a9a5347e66ee978ccc4e09b158ef54ea5a3
unknown <21PD35@cs2k16.amcs.ws2k12.psgcs.edu>
2024-01-06 15:57:09 +0530

    level1a.py succeeded

--- Level1.py
+++ Level1.py
@@ -28,7 +28,7 @@ print(demand)
 
 
 
-def calculate_savings(distance_matrix):
+"""def calculate_savings(distance_matrix):
     n = len(distance_matrix)
     savings = np.zeros((n, n))
     
@@ -45,7 +45,9 @@ def construct_routes(distance_matrix, capacity_constraint, demand):
     savings = calculate_savings(distance_matrix)
     savings_list = [(i, j, savings[i, j]) for i in range(n) for j in range(n) if i != j]
     savings_list = sorted(savings_list, key=lambda x: x[2], reverse=True)
-    
+
+    print(savings_list)
+
     visited_nodes = set()
     vehicle_routes = []
     current_route = [0]  
@@ -76,7 +78,7 @@ def construct_routes(distance_matrix, capacity_constraint, demand):
         return best_route
 
     for i, j,savings in savings_list:
-        if i not in visited_nodes and j not in visited_nodes:
+        if i not in visited_nodes and j not in visited_nodes  :
             if current_load + demand[j] <= capacity_constraint:
                 current_route.extend([i, j])
                 current_load += demand[j]
@@ -123,7 +125,77 @@ for i in result_routes:
     d['path'+str(x)]=result_path(i)
     x+=1
 result[vehicles[0]]=d
+print(result)"""
+
+sample_path=[]
+result=[]
+capacity1=[]
+cost1=[]
+def nearest_neighbor(adj_matrix):
+    n = len(adj_matrix)
+    visited = [i for i in range(n)]
+      
+    visited.remove(0)
+    print(visited)
+    capacity=600
+    while(True):
+        path = []
+        cost = 0
+        start_vertex = 0  
+        current_vertex = start_vertex
+        path.append('r'+str(current_vertex))
+        sample_path.append(current_vertex)
+        load=0
+        for _ in range(1,n):
+            l=[adj_matrix[current_vertex][i] for i in range(n) if i in visited]
+           
+            if(l!=[]):
+                nearest_neighbor = np.argmin(l)
+                next_vertex = [i for i in range(n) if i in  visited][nearest_neighbor]
+                print(nearest_neighbor,next_vertex)
+                if((load+demand[next_vertex])<=capacity):
+                    path.append('n'+str(next_vertex-1))
+                    sample_path.append(next_vertex)
+                    load+=demand[next_vertex]
+                    cost += adj_matrix[current_vertex][next_vertex]
+                    visited.remove(next_vertex)
+                    current_vertex = next_vertex
+                    print(path)
+                else:
+                    path.append('r'+str(start_vertex))
+                    sample_path.append(start_vertex)
+                    cost += adj_matrix[current_vertex][start_vertex]
+                    result.append(path)
+                    cost1.append(cost)
+                    capacity1.append(load)
+                    load=0
+                    print(path,load)  
+                    break
+            else:
+                break  
+        
+        if(visited==[]):
+            break
+    result.append(path)
+    cost1.append(cost)
+    capacity1.append(load)
+    return result,cost1,capacity
+
+adj_matrix = np.array(adj_matrix)
+final,cost1,capacity = nearest_neighbor(adj_matrix)
+print(final,cost1,capacity1)
+print("Result Routes:")
+x=1
+d={}
+result={}
+for i in final:
+    print(len(i))
+    print(i)
+    d['path'+str(x)]=(i)
+    x+=1
+result[vehicles[0]]=d
 print(result)
+
 with open("Mock-Hackathon/level1a_output.json", "w") as outfile:
     json.dump(result, outfile)
 
